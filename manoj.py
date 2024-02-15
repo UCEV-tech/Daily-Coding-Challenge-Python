f1f73c0d5c842281f9d46714bd8829b2c5a01f48
manojlearnscs <153741650+manojlearnscs@users.noreply.github.com>
2024-02-15 21:31:57 +0530

    manoj.py
    First python program

---
+++ manoj.py
@@ -0,0 +1,9 @@
+# here's how you can make your first python program
+#print() anything can be printed using this function
+#lets go
+print("iam coding!!!")
+#variable also known as storage container which holds a value , it can be anything numbers,letters.symbols,special charcters and etc
+name = "manoj"
+#here's name is the variables which stores the value "manoj"
+#you can access the value by the variable name
+print(name)
