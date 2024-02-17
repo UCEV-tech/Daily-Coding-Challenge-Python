ebd1ae5d994813f5cae0ad636d5388bb075438de
manojlearnscs <manojramesh098@gmail.com>
2024-02-17 22:47:42 +0530

    day 2 and 3 update

---
+++ DAY - 2 mano (simple math).py
@@ -0,0 +1,9 @@
+#here is how you can add two numbers in python
+a = int(input("enter the value for x"))
+b = int(input("enter the value for y"))
+print(a+b)
+# the above one gets value during runtime
+#also we can determine the value while coding 
+x = 10
+y = 20
+print(x+y)      
