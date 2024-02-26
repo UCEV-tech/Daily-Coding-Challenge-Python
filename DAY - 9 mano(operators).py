77718067ce99a99ebdb703cd320aa0dc54e958be
manojlearnscs <manojramesh098@gmail.com>
2024-02-26 19:02:37 +0530

    Day 9 completed

---
+++ DAY - 9 mano(operators).py
@@ -0,0 +1,18 @@
+"""#Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators"""
+ 
+#arithmetic operator (math operations)
+x = 10
+y = 15
+print(x + y	)	#Addition	
+print(x - y)	#Subtraction
+print(x * y)	#Multiplication
+print(x / y)    #Division
+print(x % y)    #Modulus	
+print(x ** y)   #Exponentiation
+print(x // y)   #Floor division
